83ea39f97dc1227926d8725a27ef12904c723e12
Heiko Schocher <hs@denx.de>
2024-01-25 08:24:49 +0100

    initconfighelper: fix commandline completion
    when autocompletion runs (tbot gets passed flags
    complete-testcase or complete-module) we need also
    the flags where tbot find our ini files, add this.
    
    Also we should not print anything, so suppress debug
    output.
    
    Signed-off-by: Heiko Schocher <hs@denx.de>

--- tbottest/initconfighelper.py
+++ tbottest/initconfighelper.py
@@ -1,6 +1,33 @@
+import argparse
 import tbot
 import pathlib
 import os
+import sys
+from tbot.newbot import build_parser
+
+def get_tbot_arguments():
+    """
+    get the arguments with which tbot is called
+    """
+    parser = build_parser()
+    parser.add_argument("--complete-module", help=argparse.SUPPRESS)
+    parser.add_argument("--complete-testcase", help=argparse.SUPPRESS)
+    try:
+        args = parser.parse_args(sys.argv)
+    except Exception as error:
+        print("get_tbot_arguments error parsing arguments: ", error)
+
+    # in case we do autocompletion, supress output
+    if args.complete_testcase != None:
+        tbot.selectable.printed = True
+    if args.complete_module != None:
+        tbot.selectable.printed = True
+    return args
+
+
+def get_tbot_flags():
+    args = get_tbot_arguments()
+    return args.flags
 
 
 def inifile_get_tbotfilename():
@@ -9,8 +36,9 @@ def inifile_get_tbotfilename():
     """
     # may we pass an ini file path through tbot flags
     # with for example: -f inifile_/tmp/tbot.ini
+    flags = get_tbot_flags()
     pathinifile = None
-    for f in tbot.flags:
+    for f in flags:
         if "inifile" in f:
             try:
                 pathinifile = f.split(":")[1]
@@ -37,8 +65,9 @@ def inifile_get_tbotboardfilename():
     """
     # may we pass an ini file path through tbot flags
     # with for example: -f inifile_/tmp/boardname.ini
+    flags = get_tbot_flags()
     pathinifile = None
-    for f in tbot.flags:
+    for f in flags:
         if "boardfile" in f:
             try:
                 pathinifile = f.split(":")[1]
