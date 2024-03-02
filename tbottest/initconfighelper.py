hsdenx/tbottest

commit f74e965ebf824fbec3cb458206c25ede9ecbffbe

initconfighelper.py: add pytest support
when you call tbot from pytest, you get in trouble
with commandline arguments. Suppress them, as
we need in this case only the tbot arguments passed
from commandline (by argumentfile or with -f flag)

Signed-off-by: Heiko Schocher <hs@denx.de>

tbottest/initconfighelper.py
@@ -24,6 +24,12 @@ def get_tbot_arguments():
     parser = build_parser()
     parser.add_argument("--complete-module", help=argparse.SUPPRESS)
     parser.add_argument("--complete-testcase", help=argparse.SUPPRESS)
+    if "pytest" in sys.argv[0]:
+        sys.argv = sys.argv[1:]
+        for argv in sys.argv:
+            if "@" not in argv:
+                sys.argv.remove(argv)
+
     try:
         args = parser.parse_args(sys.argv)
     except Exception as error:

commit 5a69bc3888d3ae02d583953eee51eec040879ca7

initconfighelper: fix pytest substitutions
we need to remove more arguments, as running it
with pytest 8.0.0 showed up more errors.

Fixes: commit f74e965ebf824: ("initconfighelper.py: add pytest support")

Signed-off-by: Heiko Schocher <hs@denx.de>

tbottest/initconfighelper.py
@@ -25,13 +25,18 @@ def get_tbot_arguments():
     parser.add_argument("--complete-module", help=argparse.SUPPRESS)
     parser.add_argument("--complete-testcase", help=argparse.SUPPRESS)
     if "pytest" in sys.argv[0]:
-        sys.argv = sys.argv[1:]
-        for argv in sys.argv:
+        arguments = sys.argv.copy()
+        arguments = arguments[1:]
+        i = 0
+        while i < len(arguments):
+            argv = arguments[i]
             if "@" not in argv:
-                sys.argv.remove(argv)
+                arguments.remove(argv)
+            else:
+                i += 1
 
     try:
-        args = parser.parse_args(sys.argv)
+        args = parser.parse_args(arguments)
     except Exception as error:
         print("get_tbot_arguments error parsing arguments: ", error)
 