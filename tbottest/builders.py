hsdenx/tbottest

commit b46a4b541107898f29c1cec4e584484a09bcf286

builders: introduce local machine as builder
use local machine, on which tbot is started as build host

To enable this pass flag

buildername:local

on tbot start, and add configure section

[BUILDHOST_local]

example:
[BUILDHOST_local]
name = local-build
username = hs
hostname = 192.168.1.120
\# intentionally with leading "/", so tbot works in the dir, it is started
dl_dir = work/tbottest/dl_dir
sstate_dir = work/tbottest/yocto-sstate
kas_ref_dir = work/tbottest/kas-ref-dir
workdir = work/tbottest/work
shelltype = bash
initcmd = ["uname -a"]

Signed-off-by: Heiko Schocher <hs@denx.de>

tbottest/builders.py
@@ -97,6 +97,41 @@ class genericbuilder(connector.SSHConnector, linux.Bash, linux.Builder):
         raise RuntimeError("toolchains not implemented yet, please add support!")
 
 
+class genericbuilderlocal(connector.SubprocessConnector, linux.Bash, linux.Builder):
+    sn = "BUILDHOST_local"
+    name = cfgt.config_parser.get(sn, "name")
+    dl_dir = cfgt.config_parser.get(sn, "dl_dir")
+    sstate_dir = cfgt.config_parser.get(sn, "sstate_dir")
+
+    @property
+    def kas_ref_dir(self) -> "linux.Path[genericbuilder]":
+        kas_ref_dir = cfgt.config_parser.get(self.sn, "kas_ref_dir")
+        return linux.Workdir.static(self, kas_ref_dir)
+
+    @property
+    def workdir(self) -> "linux.Path[genericbuilder]":
+        workdir = cfgt.config_parser.get(self.sn, "workdir")
+        return linux.Workdir.static(self, workdir)
+
+    def init(self) -> None:
+        if "BHINIT" not in _INIT_CACHE:
+            _INIT_CACHE["BHINIT"] = True
+            try:
+                initcmd = eval(cfgt.config_parser.get(self.sn, "initcmd"))
+            except:
+                initcmd = []
+
+            for cmd in initcmd:
+                c = []
+                for t in cmd.split(" "):
+                    c.append(t)
+
+                self.exec0(*c)
+
+    def toolchains(self) -> typing.Dict[str, linux.build.Toolchain]:
+        raise RuntimeError("toolchains not implemented yet, please add support!")
+
+
 FLAGS = {
-    "buildername": "buildername:<name of builder> searches for BUILDHOST_<name of builder> section if passed to tbot. If not searches for BUILDHOST section",
+    "buildername": "buildername:<name of builder> searches for BUILDHOST_<name of builder> section if passed to tbot. If not searches for BUILDHOST section. use name local to build on the machine, on which tbot is started",
 }

tbottest/labgeneric.py
@@ -669,8 +669,17 @@ class SSHMachine(connector.SSHConnector, linux.Bash):
 def register_machines(ctx):
     ctx.register(GenericLab, tbot.role.LabHost)
     for s in cfgt.config_parser.sections():
+        localfound = False
         if "BUILDHOST" in s:
-            ctx.register(builders.genericbuilder, tbot.role.BuildHost)
+            for f in tbot.flags:
+                if "buildername:local" in f:
+                    # build on machine, on which tbot is startet!
+                    ctx.register(builders.genericbuilderlocal, tbot.role.BuildHost)
+                    localfound = True
+
+            if not localfound:
+                ctx.register(builders.genericbuilder, tbot.role.BuildHost)
+
             return
 
 

tbottest/tbotconfig/BOARDNAME/tbot.ini
@@ -40,6 +40,20 @@ workdir = /work/big/${username}/tbot2go
 authenticator = /home/${username}/.ssh/id_rsa
 shelltype = bash
 
+# local build host, which means tbot uses the machine, on which
+# it is started as build host.
+[BUILDHOST_local]
+name = local-build
+username = hs
+hostname = 192.168.1.120
+# intentionally use relative path
+# but of course you can use here absolute paths to.
+dl_dir = work/tbottest/dl_dir
+sstate_dir = work/tbottest/yocto-sstate
+kas_ref_dir = work/tbottest/kas-ref-dir
+workdir = work/tbottest/work
+shelltype = bash
+
 [BUILDHOST_sisyphus]
 name = denx-sisyphus
 username = hs

commit f0daa28835f84e908ea362e9e84a80b98bc48d21

builders: allow tbot.ini without BUILDHOST_local section
prevent error
configparser.NoSectionError: No section: 'BUILDHOST_local'

if no BUILDHOST_local section is in tbot.ini

Fixes: b46a4b541107 ("builders: introduce local machine as builder")

Signed-off-by: Heiko <hs@denx.de>

tbottest/builders.py
@@ -102,9 +102,21 @@ class genericbuilder(connector.SSHConnector, linux.Bash, linux.Builder):
 
 class genericbuilderlocal(connector.SubprocessConnector, linux.Bash, linux.Builder):
     sn = "BUILDHOST_local"
-    name = cfgt.config_parser.get(sn, "name")
-    dl_dir = cfgt.config_parser.get(sn, "dl_dir")
-    sstate_dir = cfgt.config_parser.get(sn, "sstate_dir")
+    try:
+        name = cfgt.config_parser.get(sn, "name")
+    except:
+        name = "NOTDEFINED please add BUILDHOST_local in tbot.ini"
+        pass
+    try:
+        dl_dir = cfgt.config_parser.get(sn, "dl_dir")
+    except:
+        dl_dir = "NOTDEFINED please add BUILDHOST_local in tbot.ini"
+        pass
+    try:
+        sstate_dir = cfgt.config_parser.get(sn, "sstate_dir")
+    except:
+        sstate_dir = "NOTDEFINED please add BUILDHOST_local in tbot.ini"
+        pass
 
     @property
     def kas_ref_dir(self) -> "linux.Path[genericbuilder]":