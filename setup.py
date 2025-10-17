05b5458d6faa0abca4bbbe676bc191f0220304bf
Stefano Babic <sbabic@nabladev.com>
2025-10-17 11:56:11 +0000

    Added pyproject
    Signed-off-by: Stefano Babic <sbabic@nabladev.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tbottest",
+    version="0.1",
+    packages=find_packages(),
+    url="https://github.com/sbabic/swugenerator",
+    license="GPLv3",
+    author="Heiko Schocher",
+    author_email="hs@nabladev.com",
+    description="tools for tbot",
+    python_requires=">=3.6",
+)
