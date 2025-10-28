hsdenx/tbottest

commit 844722084c7642c9f8651b582ac2a8cccc3db619

initconfighelper: make ini files path more flexible
try to get tbotconfig path from PYTHONPATH settings.

Signed-off-by: Heiko Schocher <hs@nabladev.com>

tbottest/initconfighelper.py
@@ -72,6 +72,20 @@ def get_unique_filename_extension():
     UUID_FILENAME = str(uuid.uuid4())
     return UUID_FILENAME
 
+TBOTCONFIGPATH = None
+
+def get_tbotconfig_path():
+    global TBOTCONFIGPATH
+    if TBOTCONFIGPATH is not None:
+        return TBOTCONFIGPATH
+
+    for p in os.environ['PYTHONPATH'].split(":"):
+        if "tbotconfig" in p:
+            TBOTCONFIGPATH = p.replace("tbotconfig", "")
+            return TBOTCONFIGPATH
+
+    TBOTCONFIGPATH = os.getcwd()
+    return TBOTCONFIGPATH
 
 TBOTINIFILE = None
 
@@ -95,14 +109,15 @@ def inifile_get_tbotfilename():
             except:
                 raise RuntimeError("please use : as seperator inifile flag")
 
-    workdir = os.getcwd()
+    cfgpath = get_tbotconfig_path()
+
     if pathinifile:
         if pathinifile[0] != r"\/":  # noqa: W605
-            tbotinifile = workdir + "/" + pathinifile
+            tbotinifile = cfgpath + "/" + pathinifile
         else:
             tbotinifile = pathinifile
     else:
-        tbotinifile = workdir + "/../tbottest/tbotconfig/BOARDNAME/tbot.ini"
+        tbotinifile = cfgpath + "/../tbottest/tbotconfig/BOARDNAME/tbot.ini"
 
     uidfile = get_unique_filename_extension()
     newfilename = f"{tbotinifile}-{uidfile}"
@@ -133,14 +148,15 @@ def inifile_get_tbotboardfilename():
             except:
                 raise RuntimeError("please use : as seperator boardfile flag")
 
-    workdir = os.getcwd()
+    cfgpath = get_tbotconfig_path()
+
     if pathinifile:
         if pathinifile[0] != r"\/":  # noqa: W605
-            filename = workdir + "/" + pathinifile
+            filename = cfgpath + "/" + pathinifile
         else:
             filename = pathinifile
     else:
-        filename = workdir + "/../tbottest/tbotconfig/BOARDNAME/BOARDNAME.ini"
+        filename = cfgpath + "/../tbottest/tbotconfig/BOARDNAME/BOARDNAME.ini"
 
     uidfile = get_unique_filename_extension()
     newfilename = f"{filename}-{uidfile}"

commit a403325a6279b4fd3dd33c0b5081fe9d5028662c

initconfighelper: fix error if PYTHONPATH is not set
If PYTHONPATH is not set, get_tbotconfig_path fails with
  File "/usr/lib64/python3.7/os.py", line 679, in __getitem__
    raise KeyError(key) from None

Fix this.

Fixes: 844722084c76 ("initconfighelper: make ini files path more flexible")

Signed-off-by: Heiko Schocher <hs@nabladev.com>

tbottest/initconfighelper.py
@@ -85,10 +85,13 @@ def get_tbotconfig_path():
     except:
         pass
 
-    for p in os.environ['PYTHONPATH'].split(":"):
-        if "tbotconfig" in p:
-            TBOTCONFIGPATH = p.replace("tbotconfig", "")
-            return TBOTCONFIGPATH
+    try:
+        for p in os.environ['PYTHONPATH'].split(":"):
+            if "tbotconfig" in p:
+                TBOTCONFIGPATH = p.replace("tbotconfig", "")
+                return TBOTCONFIGPATH
+    except:
+        pass
 
     TBOTCONFIGPATH = os.getcwd()
     return TBOTCONFIGPATH