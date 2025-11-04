3790df9dafca1a20c964c82253037d32eaa469ed
Heiko Schocher <hs@nabladev.com>
2025-11-04 07:26:32 +0100

    registermap: add function for writting result into file
    add new funtion registermap_dump_register_file
    which writes the registeranalysis into a file.
    
    Signed-off-by: Heiko Schocher <hs@nabladev.com>

--- tbottest/tc/registermap.py
+++ tbottest/tc/registermap.py
@@ -1,5 +1,10 @@
 import json
-import tbot
+
+try:
+    import tbot
+except:
+    print("tbot lib not found, some functions will fail")
+    pass
 
 from tbottest.tc.common_generic import get_bit_range
 
@@ -161,5 +166,85 @@ class REGISTERMAP:
                 )
                 tbot.log.message(f"desc {bit['description']}")
             tbot.log.message("--------")
+
+        return True
+
+    def registermap_dump_register_file(self, filepath, address, val) -> bool:
+        """
+        dump the value val which is read from address into a file
+
+        This is a first proofe of concept version, the output
+        could be nicer.
+
+        :param filepath: full path to outputfile
+        :param address: hex string of address
+        :param val: value hex_string
+
+        Example output:
+
+        .. code-block:: shell
+
+            ------------------------------------------------------------------------------
+            register name: IOMUXC_SW_MUX_CTL_PAD_ENET_TXC val: 0x00000000 RM page 1438
+            ------------------------------------------------------------------------------
+            31-5   -                              val 000000000000000000000000000
+            desc This field is reserved.
+            Reserved
+
+            ...........................................
+            4      SION                           val 0
+            desc Software Input On Field.
+            Force the selected mux mode Input path no matter of MUX_MODE functionality.
+            1 ENABLED — Force input path of pad ENET_TXC
+            0 DISABLED — Input Path is determined by functionality
+            ...........................................
+            3      -                              val 0
+            desc This field is reserved.
+            Reserved
+
+            ...........................................
+            2-0    MUX_MODE                       val 000
+            desc MUX Mode Select Field.
+            Select 1 of 6 iomux modes to be used for pad: ENET_TXC.
+            000 ALT0_CCM_ENET_QOS_CLOCK_GENERATE_TX_CLK — Select mux mode: ALT0 mux port:
+            CCM_ENET_QOS_CLOCK_GENERATE_TX_CLK of instance: ccm_enet_qos_clock_generate
+            001 ALT1_ENET_QOS_TX_ER — Select mux mode: ALT1 mux port: ENET_QOS_TX_ER of instance:
+            enet_qos
+            010 ALT2_AUDIOMIX_SAI7_TX_DATA[0] — Select mux mode: ALT2 mux port:
+            AUDIOMIX_SAI7_TX_DATA00 of instance: sai7
+            101 ALT5_GPIO1_IO[23] — Select mux mode: ALT5 mux port: GPIO1_IO23 of instance: gpio1
+            110 ALT6_USDHC3_DATA1 — Select mux mode: ALT6 mux port: USDHC3_DATA1 of instance:
+            usdhc3
+
+
+
+        """
+        with open(filepath, "a", encoding="utf-8") as f:
+            f.write(
+                "------------------------------------------------------------------------------\n"
+            )
+            reg = self.registermap_search_address(address)
+            if reg is None:
+                f.write(f"registermapping for {address} not found\n")
+                return False
+
+            f.write(
+                f"register name: {reg['register']} val: {val} RM page {reg['page']}\n"
+            )
+            f.write(
+                "------------------------------------------------------------------------------\n"
+            )
+            for bit in reg["bits"]:
+                if "NXP bug not documented" in bit["range"]:
+                    f.write(
+                        f"NXPbug in doc name of field {bit['field']} desc {bit['description']}\n"
+                    )
+                elif bit["range"] == "-":
+                    f.write(f"field {bit['field']} desc {bit['description']}\n")
+                else:
+                    bits = get_bit_range(val, bit["range"])
+                    f.write(f"{bit['range']:6} {bit['field']:30} val {bits:6}\n")
+                    f.write(f"desc {bit['description']}\n")
+                f.write("...........................................\n")
 
         return True
