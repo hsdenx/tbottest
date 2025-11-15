24134f5162539c4c084d08baca31b0afc4b44f39
Heiko Schocher <hs@nabladev.com>
2025-11-15 12:32:12 +0100

    ci: check_lab_locking.sh: add ERROR string to output
    print errors with ERROR, so we can easier search for errors in
    log.
    
    Signed-off-by: Heiko Schocher <hs@nabladev.com>

--- scripts/ci/check_lab_locking.sh
+++ scripts/ci/check_lab_locking.sh
@@ -16,52 +16,52 @@ sed -i '/uselocking/c\uselocking = yes' $tbotinifile
 echo "---- call without lockid, must fail ----"
 tbottest/newtbot_starter.py @tbotconfig/foo/args/argsfoo-noeth tbotconfig.ci.testlab.uname > lab-lock-log
 if [ "$?" != "1" ];then
-	echo "Locking into Lab without passing lock id must fail"
+	echo "ERROR: Locking into Lab without passing lock id must fail"
 	RET=1
 fi
 grep "NO LABLOCKID passed" lab-lock-log
 if [ "$?" != "0" ];then
-	echo "Locking into Lab without passing lock id must fail"
+	echo "ERROR: Locking into Lab without passing lock id must fail"
 	RET=1
 fi
 
 echo "---- call with lockid foobar ----"
 tbottest/newtbot_starter.py @tbotconfig/foo/args/argsfoo-noeth -f lablockid:foobar tbotconfig.ci.testlab.uname
 if [ "$?" != "0" ];then
-	echo "Locking into Lab with no lock must work"
+	echo "ERROR: Locking into Lab with no lock must work"
 	RET=1
 fi
 
 echo "---- call again with lockid foobar ----"
 tbottest/newtbot_starter.py @tbotconfig/foo/args/argsfoo-noeth -f lablockid:foobar tbotconfig.ci.testlab.uname
 if [ "$?" != "0" ];then
-	echo "Locking into Lab with lock foobar must work"
+	echo "ERROR: Locking into Lab with lock foobar must work"
 	RET=1
 fi
 
 echo "---- call with wrong lockid must fail ----"
 tbottest/newtbot_starter.py @tbotconfig/foo/args/argsfoo-noeth -f lablockid:foobar2 tbotconfig.ci.testlab.uname > lab-lock-log
 if [ "$?" != "1" ];then
-	echo "Locking into Lab with wrong lock foobar2 must fail"
+	echo "ERROR: Locking into Lab with wrong lock foobar2 must fail"
 	RET=1
 fi
 
 echo "---- Delete the lock with wrong lockid must fail ----"
 tbottest/newtbot_starter.py @tbotconfig/foo/args/argsfoo-noeth -f lablockid:foobar tbottest.common.boardlocking.lab_rm_lock
 if [ "$?" != "1" ];then
-	echo "remove Locking with wrong lock foobar2 must fail"
+	echo "ERROR: remove Locking with wrong lock foobar2 must fail"
 	RET=1
 fi
 grep "passed lockid foobar2 is not the same as lockid" lab-lock-log
 if [ "$?" != "0" ];then
-	echo "remove Locking with wrong lock foobar2 must fail"
+	echo "ERROR: remove Locking with wrong lock foobar2 must fail"
 	RET=1
 fi
 
 echo "---- Delete lock with correct lockid must work ----"
 tbottest/newtbot_starter.py @tbotconfig/foo/args/argsfoo-noeth -f lablockid:foobar tbottest.common.boardlocking.lab_rm_lock
 if [ "$?" != "0" ];then
-	echo "remove Locking with correct lock foobar must work"
+	echo "ERROR: remove Locking with correct lock foobar must work"
 	RET=1
 fi
 
