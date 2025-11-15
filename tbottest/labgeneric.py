hsdenx/tbottest

commit 57e28cbd7f15b69632602d0d0fe638f19c57b3c1

labgeneric: set ssh interface boardspecific
currently for logging in into DUT through ssh
"eth0" interface is fix in code. SSH machine gets
from that configuration the boards IP address for
ssh login. Make it configurable through a board specific
function board_detect_lx_ethernet_interface()

Signed-off-by: Heiko Schocher <hs@nabladev.com>

tbottest/labgeneric.py
@@ -12,6 +12,7 @@ from tbottest import powercontrol
 from tbottest import machineinit
 from tbottest.common.boardlocking import lab_get_lock
 from tbottest.dynamicimport import get_boardcallback_import
+from tbottest.dynamicimport import get_boardmodule_import
 
 import tbottest.initconfighelper as inithelper
 
@@ -25,15 +26,6 @@ if cfgt.shelltype == "bash":
     LAB_LINUX_SHELL = linux.Bash
 else:
     LAB_LINUX_SHELL = linux.Ash
-
-# for ssh machine
-IP_BOARD_SSH_INTERFACE = "eth0"
-try:
-    ssh_ip_section = f"IPSETUP_{ini.generic_get_boardname()}_{IP_BOARD_SSH_INTERFACE}"
-    IP_BOARD = cfgt.config_parser.get(ssh_ip_section, "ipaddr")
-except:
-    IP_BOARD = None
-
 
 class boardPicocomConnector(PicocomConnector):
     bn = ini.generic_get_boardname()
@@ -80,7 +72,19 @@ class boardSSHConnector(connector.SSHConnector):
         ignore_hostkey = True
 
     username = ini.init_get_config(cfgp, "linux_user", "root")
-    if IP_BOARD is None:
+    # hostname for ssh machine to board
+    try:
+        board_detect_lx_ethernet_interface = getattr(get_boardmodule_import(), "board_detect_lx_ethernet_interface")
+        IP_BOARD_SSH_INTERFACE = board_detect_lx_ethernet_interface()
+        tbot.log.message(tbot.log.c(f"Use {IP_BOARD_SSH_INTERFACE} interface for ssh").yellow)
+    except:
+        IP_BOARD_SSH_INTERFACE = "eth1"
+        tbot.log.message(tbot.log.c(f"Use default {IP_BOARD_SSH_INTERFACE} interface for ssh, make it boardspecific through board_detect_lx_ethernet_interface()").yellow)
+
+    try:
+        ssh_ip_section = f"IPSETUP_{ini.generic_get_boardname()}_{IP_BOARD_SSH_INTERFACE}"
+        IP_BOARD = cfgt.config_parser.get(ssh_ip_section, "ipaddr")
+    except:
         RuntimeError(
             f"Please set ipaddr for ssh machine. Expect section {ssh_ip_section} with key ipaddr"
         )

commit dc674c27295ff65f39ae1bda52788590a90296f0

labgeneric: fix CI commandline completition
Fixes: 57e28cb: ("labgeneric: set ssh interface boardspecific")

This commit prints a help text, which breaks
commandline completion. So remove it.

Signed-off-by: Heiko Schocher <hs@nabladev.com>

tbottest/labgeneric.py
@@ -86,11 +86,6 @@ class boardSSHConnector(connector.SSHConnector):
         )
     except:
         IP_BOARD_SSH_INTERFACE = "eth0"
-        tbot.log.message(
-            tbot.log.c(
-                f"Use default {IP_BOARD_SSH_INTERFACE} interface for ssh, make it boardspecific through board_detect_lx_ethernet_interface()"
-            ).yellow
-        )
 
     try:
         ssh_ip_section = f"IPSETUP_{ini.generic_get_boardname()}_{IP_BOARD_SSH_INTERFACE}"